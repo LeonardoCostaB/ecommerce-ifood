a7eacaaf58d290282914a895302c567aa6fcaf33
Leonardo <leonardo_barrosocosta@hotmail.com>
2022-08-30 11:45:47 -0300

    finalizando sessao de sobre nos e banner no desktop

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,17 @@
+import { Link } from "react-router-dom";
+''
 import { Header } from "../../components/Header";
 import { SwiperSlider } from "../../components/SwiperSlide";
 
 import style from "./style.module.scss";
 
+import logo from "../../assets/svgs/logo.svg";
+import logoAntigo from "../../assets/imgs/logo-antigo.png";
 import alema from "../../assets/imgs/alema.jpg";
-import chocolate from "../../assets/imgs/chocolate.jpg";
+import tortaFrango from "../../assets/imgs/torta-de-frango.jpg";
 import cremeTrufada from "../../assets/imgs/creme-trufada.jpg";
-import ferrero from "../../assets/imgs/ferrero.jpg";
+import petit from "../../assets/imgs/petit.jpg";
+import loja from "../../assets/imgs/loja.jpeg";
 
 const products = {
   sweetPie: {
@@ -54,14 +59,33 @@ export function Home() {
          <main>
             <section className={style["apresentation-section"]}>
                <div className={style["title-apresentation"]}>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste consequuntur libero asperiores delectus, pariatur natus dolores repudiandae temporibus odit excepturi adipisci rem dicta harum ex tenetur inventore tempora assumenda explicabo.
+                  <h1 className={style.title}>
+                     Sejam Bem Vindos a <strong className="dolce-cannella">Dolce Cannela</strong>
+                  </h1>
+
+                  <p className={style.description}>
+                     Casa das Tortas agora é Dolce Cannella <br />
+                     Dolce Cannella agora é Casa das Tortas
+                  </p>
+
+                  <div className={style["comparison-soon"]}>
+                     <img
+                        src={logoAntigo}
+                        alt=""
+                        className={style["old-logo"]}
+                     />
+
+                     <img src={logo} alt="" />
+                  </div>
                </div>
 
                <SwiperSlider />
             </section>
 
             <section className={style["product-list"]}>
-               <h2 className={style["section-title"]}>Confira todos os nossos sabores</h2>
+               <h2 className={style["section-title"]}>
+                  Venha conferir os nossos produtos
+               </h2>
 
                <div className={style.container}>
                   <div className={style.product}>
@@ -80,7 +104,7 @@ export function Home() {
 
                   <div className={style.product}>
                      <a href="">
-                        <img src={chocolate} alt="" />
+                        <img src={tortaFrango} alt="" />
                      </a>
 
                      <div className={style["product-access"]}>
@@ -108,7 +132,7 @@ export function Home() {
 
                   <div className={style.product}>
                      <a href="">
-                        <img src={ferrero} alt="" />
+                        <img src={petit} alt="" />
                      </a>
 
                      <div className={style["product-access"]}>
@@ -121,8 +145,36 @@ export function Home() {
                   </div>
                </div>
             </section>
-         </main>
 
+            <section className={style["our-story"]}>
+               <h2 className={style["section-title"]}>
+                  Um pouco da nossa história
+               </h2>
+
+               <div className={style["container"]}>
+                  <img
+                     src={loja}
+                     alt="Venha conhecer nossa loja"
+                     className={style["shop-img"]}
+                  />
+
+                  <div className={style["description"]}>
+                     <p className={style.text}>
+                        <span>Produzindo tortas e bolos há mais de 25 anos estamos <br /> agora atualizando a nossa marca</span>
+
+                        De "Casa das Tortas" para <strong className="dolce-cannella">Dolce Cannella</strong> Venha nos <br /> conhecer e os nossos produtos.
+                     </p>
+
+                     <Link
+                        to="/about-us"
+                        className={style["access-aboutUs"]}
+                     >
+                        Saiba Mais
+                     </Link>
+                  </div>
+               </div>
+            </section>
+         </main>
       </>
    );
 };
