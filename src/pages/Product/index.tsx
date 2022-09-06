12ace60d16b084c42acbe5bf9c9e73ab01be7d5b
Leonardo <leonardo_barrosocosta@hotmail.com>
2022-09-06 20:42:50 -0300

    corrigindo erro no before do menu no mobile e alterando titulo da pagina de produtos

--- src/pages/Product/index.tsx
+++ src/pages/Product/index.tsx
@@ -2,12 +2,11 @@ import { useParams } from "react-router-dom";
 import { gql, useQuery } from "@apollo/client";
 
 import { Header } from "../../components/Header";
+import { BreadCrump } from "../../components/BreadCrump";
 import { ProductApresentation } from "../../components/ProductApresentation";
 import { Footer } from "../../components/Footer";
 
 import style from "./style.module.scss";
-
-import torta from "../../assets/imgs/torta-africana.jpg";
 
 const GET_PRODUCT_BY_CATEGORY_QUERY = gql`
    query GetProductByCategory ($category: String) {
@@ -34,6 +33,8 @@ interface GetProductBySlugResponse {
    }[]
 }
 
+let titleSection: string;
+
 export function Product () {
    const { product } = useParams<{ product: string }>();
    const { data, loading } = useQuery<GetProductBySlugResponse>(GET_PRODUCT_BY_CATEGORY_QUERY, {
@@ -42,13 +43,27 @@ export function Product () {
       }
    });
 
+   if(product == "torta-doce") {
+      titleSection = "Tortas Doces"
+   } else if(product == "torta-salgada") {
+      titleSection = "Tortas Salgadas"
+   } else if(product == "bolo") {
+      titleSection = "Bolos"
+   } else if(product == "individual") {
+      titleSection = "Individuais"
+   }
+
    return (
       <>
          <Header />
 
+         <BreadCrump
+            pageName={titleSection}
+         />
+
          <main className={style.main}>
             <h1 className={style.title}>
-               Sessão { product?.replace("-", " ") }
+               { titleSection }
             </h1>
 
             <div className={style.container}>
