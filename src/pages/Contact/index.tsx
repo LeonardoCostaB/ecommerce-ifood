b302aa06ff73e3323168f146eea85af132ff2d53
Leonardo <leonardo_barrosocosta@hotmail.com>
2022-09-07 19:55:32 -0300

    estruturando e estilizando pagina de contato

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -2,6 +2,8 @@ import { Header } from "../../components/Header";
 import { Footer } from "../../components/Footer";
 
 import style from "./style.module.scss";
+import { GoogleMaps } from "../../components/GoogleMaps";
+import { Envelope, MapPin, WhatsappLogo } from "phosphor-react";
 
 export function Contact() {
    return (
@@ -9,7 +11,49 @@ export function Contact() {
          <Header />
 
          <main className={style.contact}>
-            Hello word
+            <h1 className={style.title}>Contato</h1>
+
+            <div className={style.container}>
+               <div className={style["container-map"]}>
+                  <GoogleMaps />
+               </div>
+
+               <div className={style.informations}>
+                  <div className={style["informations-wrapper"]}>
+                     <address className={style.address}>
+                        <a
+                           href="mailto:contato@dolcecannella.com.br"
+                           target="_blank"
+                           className={style.email}
+                        >
+                           <Envelope size={24} className={style["email-icon"]}/>
+                           <span>contato@dolcecannella.com.br</span>
+                        </a>
+                     </address>
+
+                     <address className={style.address}>
+                        <a
+                           href="https://contate.me/dolcecannella"
+                           target="_blank"
+                           className={style.whatsapp}
+                        >
+                           <WhatsappLogo size={30} className={style["whatsapp-icon"]} />
+
+                           <span>Dolce Cannella</span>
+                        </a>
+                     </address>
+
+                     <span className={style.options}>ou nos visite</span>
+
+                     <address className={style.address}>
+                        <MapPin size={24} />
+                        <span>Avenida Jacaúna 977</span>
+                     </address>
+                  </div>
+               </div>
+
+            </div>
+
          </main>
 
          <Footer />
